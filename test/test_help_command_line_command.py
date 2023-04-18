562fb4c9dc65a6bd4e93873629152b2d26b748ea
mbui <mbuiedu@gmail.com>
2023-04-18 18:55:59 -0400

    modify test for helpcl cmd to verify list of lines from output text file

--- test/test_help_command_line_command.py
+++ test/test_help_command_line_command.py
@@ -23,26 +23,34 @@ class HelpCommandLineCommandTest(CommandTest):
         self.assertFalse(self.errors)
 
         os.system('topydo help cl > help_cl_cmd_output.txt')
-        help_cl_cmd_output = str(open('help_cl_cmd_output.txt').readlines())
+        help_cl_cmd_output = open('help_cl_cmd_output.txt').readlines()
 
-        self.assertIn('Commandline shortcuts', help_cl_cmd_output)
-        self.assertIn("The commandline, activated when pressing ':', supports the following keys:", help_cl_cmd_output)
-        self.assertIn('KEY     DESCRIPTION', help_cl_cmd_output)
-        self.assertIn('Ctrl-a  Move to the beginning', help_cl_cmd_output)
-        self.assertIn('Ctrl-e  Move to the end', help_cl_cmd_output)
-        self.assertIn('Ctrl-u  Delete from the cursor to the beginning', help_cl_cmd_output)
-        self.assertIn('Ctrl-k  Delete from the cursor up to the end', help_cl_cmd_output)
+        self.assertListEqual(['Commandline shortcuts\n',
+                              '\n',
+                              "The commandline, activated when pressing ':', supports the following keys:\n",
+                              '      \n',
+                              '  KEY     DESCRIPTION                              \n',
+                              '    \n',
+                              '  Ctrl-a  Move to the beginning                    \n',
+                              '  Ctrl-e  Move to the end                          \n',
+                              '  Ctrl-u  Delete from the cursor to the beginning  \n',
+                              '  Ctrl-k  Delete from the cursor up to the end     \n',
+                              '\n'], help_cl_cmd_output)
 
         os.system('topydo helpcl > help_cl_cmd_output.txt')
-        help_cl_cmd_output = str(open('help_cl_cmd_output.txt').readlines())
+        help_cl_cmd_output = open('help_cl_cmd_output.txt').readlines()
 
-        self.assertIn('Commandline shortcuts', help_cl_cmd_output)
-        self.assertIn("The commandline, activated when pressing ':', supports the following keys:", help_cl_cmd_output)
-        self.assertIn('KEY     DESCRIPTION', help_cl_cmd_output)
-        self.assertIn('Ctrl-a  Move to the beginning', help_cl_cmd_output)
-        self.assertIn('Ctrl-e  Move to the end', help_cl_cmd_output)
-        self.assertIn('Ctrl-u  Delete from the cursor to the beginning', help_cl_cmd_output)
-        self.assertIn('Ctrl-k  Delete from the cursor up to the end', help_cl_cmd_output)
+        self.assertListEqual(['Commandline shortcuts\n',
+                              '\n',
+                              "The commandline, activated when pressing ':', supports the following keys:\n",
+                              '      \n',
+                              '  KEY     DESCRIPTION                              \n',
+                              '    \n',
+                              '  Ctrl-a  Move to the beginning                    \n',
+                              '  Ctrl-e  Move to the end                          \n',
+                              '  Ctrl-u  Delete from the cursor to the beginning  \n',
+                              '  Ctrl-k  Delete from the cursor up to the end     \n',
+                              '\n'], help_cl_cmd_output)
 
         os.remove('help_cl_cmd_output.txt')
 
