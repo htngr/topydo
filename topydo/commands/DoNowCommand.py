c6eb9c4c0a587cb694be1390d9db8721840bca52
mbui <mbuiedu@gmail.com>
2023-04-19 17:50:18 -0400

    correct parent class, refactor execute method to do error handling

--- topydo/commands/DoNowCommand.py
+++ topydo/commands/DoNowCommand.py
@@ -1,39 +1,42 @@
-from topydo.lib.DCommand import DCommand
+from topydo.lib.Command import Command, InvalidCommandArgument
+from topydo.lib.TodoListBase import InvalidTodoException
 from topydo.commands.TagCommand import TagCommand
 import time
 
 
-class DoNowCommand(DCommand):
+class DoNowCommand(Command):
     def __init__(self, p_args, p_todolist, #pragma: no branch
                  p_out=lambda a: None,
                  p_err=lambda a: None,
                  p_prompt=lambda a: None):
         super().__init__(p_args, p_todolist, p_out, p_err, p_prompt)
 
-        self.todo_id = p_args[0]
-
-    def prefix(self):
-        return 'DOING: '
-
-    def execute_specific_core(self, p_todo):
-        # min_value = p_todo.tag_values('min')
-        min_value = 0 if len(p_todo.tag_values('min')) == 0 else int(p_todo.tag_values('min')[0])
-        # print(min_value)
-        min_elapsed = 0
+    def execute(self):
+        if not super().execute():
+            return False
 
         try:
-            while True:
-                time.sleep(1 * 60)
-                min_elapsed += 1
-                # print(min_elapsed)
-        except KeyboardInterrupt:
-            TagCommand([self.todo_id, 'min', f'{min_value + min_elapsed}'], self.todolist).execute()
-            self.out(f'\n{min_elapsed} MINUTE(S) PASSED\n'
-                     f'UPDATED TODO: |{self.todo_id}| {self.printer.print_todo(p_todo)}')
+            todo_id = self.argument(0)
+            todo = self.todolist.todo(todo_id)
 
-    def execute_specific(self, p_todo):
-        self.out(self.prefix() + self.printer.print_todo(p_todo))
-        self.execute_specific_core(p_todo)
+            self.out(f'DOING: {self.printer.print_todo(todo)}')
+            min_value = 0 if len(todo.tag_values('min')) == 0 else int(todo.tag_values('min')[0])
+            # print(min_value)
+            min_elapsed = 0
+
+            try:
+                while True:
+                    time.sleep(1 * 60)
+                    min_elapsed += 1
+                    # print(min_elapsed)
+            except KeyboardInterrupt:
+                TagCommand([todo_id, 'min', f'{min_value + min_elapsed}'], self.todolist).execute()
+                self.out(f'\n{min_elapsed} MINUTE(S) PASSED\n'
+                         f'UPDATED TODO: |{todo_id}| {self.printer.print_todo(todo)}')
+        except InvalidCommandArgument:
+            self.error(self.usage())
+        except InvalidTodoException:
+            self.error("Invalid todo number given.")
 
     def usage(self):
         return """Synopsis: donow <NUMBER>"""
