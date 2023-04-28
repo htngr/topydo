f7e9912b19428e55b90789e04338e286fc42662c
s5teixeira <steixeira1555@gmail.com>
2023-04-27 21:04:17 -0400

    added priority test functions

--- test/test_do_command.py
+++ test/test_do_command.py
@@ -822,6 +822,54 @@ The following todo item(s) became active:
                                       f'Completed: x (B) {self.today} Foo id:1\n')
         self.assertFalse(self.errors)
 
+    def test_keep_priority_do_subtasks_force2(self):
+        def prompt(p_prompt):
+            prompt.prompt_shown = True
+
+            prompt.prompt_shown = False
+
+            config('test/data/keeppriority0.conf')
+
+            command = DoCommand(['--force', '2'], self.todolist_with_priorities, self.out, self.error, prompt)
+            command.execute()
+            command.execute_post_archive_actions()
+
+            self.assertFalse(prompt.prompt_shown)
+            self.assertTrue(self.todolist_with_priorities.dirty)
+            self.assertTrue(self.todolist_with_priorities.todo(2).is_completed())
+            self.assertFalse(self.todolist_with_priorities.todo(3).is_completed())
+            self.assertFalse(self.todolist_with_priorities.todo(4).is_completed())
+            self.assertEqual(self.output, f'|  3| (B) Bar p:1\n'
+                                          f'|  4| (B) Baz p:1\n'
+                                          f'Completed: x {self.today} Foo id:1\n')
+            self.assertFalse(self.errors)
+
+    def test_keep_priority_do_custom_date1(self):
+        config('test/data/keeppriority1.conf')
+
+        command = DoCommand(['-d', '2014-11-18', '4'], self.todolist_with_priorities, self.out, self.error)
+        command.execute()
+        command.execute_post_archive_actions()
+
+        self.assertTrue(self.todolist_with_priorities.dirty)
+        self.assertTrue(self.todolist_with_priorities.todo(4).is_completed())
+        self.assertEqual(self.todolist_with_priorities.todo(4).completion_date(), date(2014, 11, 18))
+        self.assertEqual(self.output, 'Completed: x (B) 2014-11-18 Baz p:1\n')
+        self.assertFalse(self.errors)
+
+    def test_keep_priority_do_custom_date2(self):
+        config('test/data/keeppriority0.conf')
+
+        command = DoCommand(['-d', '2014-11-18', '4'], self.todolist_with_priorities, self.out, self.error)
+        command.execute()
+        command.execute_post_archive_actions()
+
+        self.assertTrue(self.todolist_with_priorities.dirty)
+        self.assertTrue(self.todolist_with_priorities.todo(4).is_completed())
+        self.assertEqual(self.todolist_with_priorities.todo(4).completion_date(), date(2014, 11, 18))
+        self.assertEqual(self.output, 'Completed: x 2014-11-18 Baz p:1\n')
+        self.assertFalse(self.errors)
+
     def test_do_name(self):
         name = DoCommand.name()
 
